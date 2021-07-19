119f6c231a086d3723c34fd60190bc80fedba301
AriqoAzhim <ariqo.azhim@gmail.com>
2021-07-19 21:35:39 +1000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mtrn4230/4230_stuff/finalprobs/catkin_ws/src
+# Build directory: /home/mtrn4230/4230_stuff/finalprobs/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("field")
---
+++ build/field/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mtrn4230/4230_stuff/finalprobs/catkin_ws/src/field
+# Build directory: /home/mtrn4230/4230_stuff/finalprobs/catkin_ws/build/field
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
